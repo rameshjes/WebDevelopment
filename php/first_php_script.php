ec13c8c3ad93c4a5965c3efc9913b2c8a94e1e1e
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-08-17 10:53:26 +0200

    first php script added

---
+++ php/first_php_script.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+	    <p>
+	      <?php
+	      $myName = "Ramesh <br> <b> Kumar </b>";
+	      echo $myName;
+	      ?>
+	    </p>
+    </body>
+</html>
