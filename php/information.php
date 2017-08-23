f46ab2fccbbe9be963a518ffe6cee87f826f158a
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-08-23 23:05:18 +0200

    second form controlled via post method added

--- php/information.php
+++ php/information.php
@@ -13,8 +13,24 @@
       <b> Height: <b> <input type="text" name="height_id">
       <br> <br>
       <input type="Submit">
+      <br><br><br>
 
     </form>
+
+   <p> This form is controlled using post method from $_post_example.php </p>
+   <p> user name is "ramesh" and password is "1234" </p>
+   <p> If user name and password matches then login successfully else Invalid
+           </p>
+
+   <form method="post" action="$_Post_example.php">
+     <b> User Name: <b> <input type="text" name="username">
+     <br> <br>
+     <b> Password: <b> <input type="password" name="password">
+     <br> <br>
+     <input type="Submit">
+
+   </form>
+
 
     </body>
 </html>
