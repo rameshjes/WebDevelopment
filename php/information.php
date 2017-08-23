b3b3fe50a214b405911a3fac6b6e4fdb387ac156
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-08-23 22:45:48 +0200

    client form added

---
+++ php/information.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <h1> Information about Clients   </h1>
+	</head>
+	<body>
+    <form method="get" action="$_Get_example.php">
+      <b> Name: <b> <input type="text" name="name_id">
+      <br> <br>
+      <b> Age: <b> <input type="text" name="age_id">
+      <br> <br>
+      <b> Height: <b> <input type="text" name="height_id">
+      <br> <br>
+      <input type="Submit">
+
+    </form>
+
+    </body>
+</html>
