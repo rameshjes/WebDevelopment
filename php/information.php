c454c3ac66174aa1720bb35a4b9c2bd412a1db11
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-08-23 22:48:11 +0200

    comment lines added

--- php/information.php
+++ php/information.php
@@ -3,6 +3,7 @@
 	<head>
     <h1> Information about Clients   </h1>
 	</head>
+  <p> This form is controlled using $_Get_example.php file    </p>
 	<body>
     <form method="get" action="$_Get_example.php">
       <b> Name: <b> <input type="text" name="name_id">
